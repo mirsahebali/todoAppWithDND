f23251619fd2e519ac8580e00b3daeb84c6328ef
Mir Saheb Ali <mirsahebali204@gmail.com>
2023-04-01 05:37:33 +0530

    debugging #2

--- src/pages/components/Todo.tsx
+++ src/pages/components/Todo.tsx
@@ -1,5 +1,5 @@
 import {FaRegTrashAlt} from 'react-icons/fa'
-export default function Todo({todo, text,  completed, toggleComplete, deleteTodo, prop1, prop2, inRef}:{todo: any, completed: boolean, toggleComplete:any, text: string, deleteTodo:any, prop1: any, prop2: any, inRef:any }) {
+export default function Todo({todo, text,  completed, toggleComplete, deleteTodo, prop1, prop2, inRef}:any) {
 const styles:any = {
 li: `flex items-center justify-between bg-slate-100 rounded-lg shadow-sm p-2 my-2`,
 liComplete: `flex items-center justify-between bg-slate-100 rounded-lg shadow-sm p-2 my-2 line-through  cursor-pointer`,
@@ -10,7 +10,7 @@ button: `bg-red-500 hover:bg-red-900 duration-300 text-white rounded-lg p-2`,
 }
 return <li {...prop1} {...prop2} ref={inRef} className={completed? styles.liComplete: styles.li}>
 <div className={styles.row}>
-          <input onChange={()=> toggleComplete(todo)} type="checkbox" placeholder="todo" checked={completed ? 'checked': ''}/>
+          <input onChange={()=> toggleComplete(todo)} type="checkbox" placeholder="todo" checked={completed ? true: false}/>
           <p onClick={()=> toggleComplete(todo)} className={completed? styles.textComplete:  styles.text}>{text}</p>
 </div>
 <button className={styles.button} onClick={()=> deleteTodo(todo)}>{<FaRegTrashAlt/>}</button>
